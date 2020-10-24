4fe91d607fa3a2396a60ba03daca9063e25644dc
pxkmsw <crazyengineer1004@gmail.com>
2020-10-24 20:17:23 +0200

    last minute change

--- auth/tests/routes/currentUser.test.ts
+++ auth/tests/routes/currentUser.test.ts
@@ -4,11 +4,7 @@ import { app } from '../../src/app';
 it('responds with details about the current user', async () => {
   const cookie = await global.signup();
 
-  const response = await request(app)
-    .get('/api/users/currentuser')
-    .set('Cookie', cookie)
-    .send()
-    .expect(200);
+  const response = await request(app).get('/api/users/currentuser').set('Cookie', cookie).send().expect(400);
 
   expect(response.body.currentUser.email).toEqual('ts@ts.com');
 });
