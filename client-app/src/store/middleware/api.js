33dbfb28f65575cd3d669598f88fb0cdf8f83556
pxkmsw <crazyengineer1004@gmail.com>
2020-11-08 20:24:43 +0100

    change port for bugs service

--- client-app/src/store/middleware/api.js
+++ client-app/src/store/middleware/api.js
@@ -11,7 +11,7 @@ const api = ({ dispatch }) => next => async action => {
 
   try {
     const response = await axios.request({
-      baseURL: 'http://localhost:9001/api',
+      baseURL: 'http://www.ticketing-dev-micro.xyz/api/bugs',
       url,
       method,
       data,
